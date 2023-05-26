c8bee527412e21ad8022e9200480146b77da4544
kyryloponomarev <kyryloponomarev0509@gmail.com>
2023-05-26 22:57:07 +0900

    xcv

--- src/type.ts
+++ src/type.ts
@@ -3,4 +3,4 @@ export interface IState {
     component: any;
     [index: string]: any;
 }
-sdsddsddsfewadsfadfasdfsdfasdsdfsdfasdfasdfdsasdfsfdasdfdfsadsfsaasdfd
+sdsddsddsfewadsfadfasdfsdfasdsdfsdfasdfasdfdsasdfsfdasdfdfsadsfsaasdfdasdfd
