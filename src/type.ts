5e9823eea1df8a7a7ba2c0acad00ebed2f82b8c5
kyryloponomarev <kyryloponomarev0509@gmail.com>
2023-05-26 22:39:49 +0900

    as

--- src/type.ts
+++ src/type.ts
@@ -1,3 +1,10 @@
 export interface IState {
     show: Function;
     component: any;
+    [index: string]: any;
+}
+
+export interface GlobalStateMapping {
+    currentState: string;
+    [index: string]: any;
+}
