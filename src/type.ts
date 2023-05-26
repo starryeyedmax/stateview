387f4af3556c58a1eab3202cf23ded76a8c58dee
kyryloponomarev <kyryloponomarev0509@gmail.com>
2023-05-26 23:02:15 +0900

    ve

--- src/type.ts
+++ src/type.ts
@@ -3,4 +3,4 @@ export interface IState {
     component: any;asdfsadf
     [index: string]: any;
 }
-asdfsadfasdgeasdfasdgsdaasdfasdfasdfasdfasdfasdfsdfadsfsasdf
+asdfsadfasdgeasdfasdgsdaasdfasdfasdfasdfasdfasdfsdfadsfsasdfasdf
